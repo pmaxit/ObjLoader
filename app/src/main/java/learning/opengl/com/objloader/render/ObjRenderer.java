c09a630974f82ef147cb4b3d3323f11049cf83f3
puneet <puneet@puneets-MacBook-Pro.local>
2015-10-18 01:31:54 -0500

    First commit

---
+++ app/src/main/java/learning/opengl/com/objloader/render/ObjRenderer.java
@@ -0,0 +1,39 @@
+package learning.opengl.com.objloader.render;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by puneet on 10/18/15.
+ */
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+public class ObjRenderer implements GLSurfaceView.Renderer {
+
+    private Context context;
+    public ObjRenderer(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(0f, 0f, 0f, 1f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
